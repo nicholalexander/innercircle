95a6f479a6ae070b200229de04b6ff353cbb4dd0
Arun Umapathy <arunumap@gmail.com>
2014-02-12 17:10:04 -0500

    reworking capistrano to push

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -26,14 +26,14 @@ server 'innercircle.nycdevshop.com', user: 'railsapps', roles: %w{web app} #, my
    auth_methods: %w(password)
  }
 # and/or per server
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+server 'innercircle.nycdevshop.com',
+  user: 'railsapps',
+  roles: %w{web app},
+  ssh_options: {
+    user: 'railsapps', # overrides user setting above
+    keys: %w(/home/railsapps/.ssh/id_rsa),
+    forward_agent: false,
+    auth_methods: %w(publickey password)
+    password: 'please use keys'
+  }
 # setting per server overrides global ssh_options
