3d9ed9c493765d54256c0c34da58b0b5e24c6298
Nichol Alexander <nichol.alexander@gmail.com>
2014-02-19 14:09:27 -0500

    untracked changes

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){ 
 	$("#new_comment").submit(function(e) {
 		e.preventDefault();
-		console.log("yes")
-		console.log(this)
 		var parentInput = $(this).parent();
-		console.log(parentInput)
 		var content = parentInput.find("#comment_text").val();
-		console.log(content)
+
 		$.ajax({
 			type: 'post',
 			url: this.action, 
-			data: {comment:
-				      {text: content, 
-				       photo_id: $(this).find('#photo_id').val()
-				      }
-				    },
-			dataType: 'script'
-			// success: function(data) {
-			// 	$('.comments').html(data)
-			
+			data: {
+							comment:
+								{
+									text: content, 
+									photo_id: $(this).find('#photo_id').val()
+								}
+						},
+			dataType: 'script',
+			success: function(data) {
+				console.log(data);
+				})
 		});
+	
 	});
 
 });
