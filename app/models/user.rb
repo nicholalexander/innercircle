cc0c9127b90b9c4588d8ca935db0868e3dd2038c
Arun Umapathy <arunumap@gmail.com>
2014-02-20 11:03:47 -0500

    finished avatar css on comment display

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	has_attached_file :avatar, :styles => { :thumb => "300x300>" }
+	has_attached_file :avatar, :styles => { :thumb => "75x75>" }
   
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   has_many :photos
